ea32b64ee3e3d80ea3909ecff680be47ca94020d
itsprince05 <102887695+itsprince05@users.noreply.github.com>
2026-06-07 08:34:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ AVATARS_DIR = os.path.join(BOT_DIR, "avatars")
 ADMINS_FILE = os.path.join(BOT_DIR, "admins.json")
 os.makedirs(AVATARS_DIR, exist_ok=True)
 
-HTML_TEMPLATE = ""
+HTML_TEMPLATE = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
